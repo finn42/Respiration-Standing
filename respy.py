375f03eb0d6230df13b35b2c68d757f9692bbc4e
Finn Upham <finn.upham@gmail.com>
2021-01-22 06:45:25 -0500

    Phase alignment assessments with activity analysis

--- respy.py
+++ respy.py
@@ -327,3 +327,157 @@ def Breath_Continues_Features(sig,scaling=0,filtered=False,interp_style='previou
         RespFeatures[col]=f(RespFeatures.index)
 
     return RespFeatures
+
+
+def Resp_phases(sig,scale_high=0.7, scale_low = 0.3):
+    ''' Evaluates the respiratory phase components on a respiration signal.
+    Inputs: sig - dataframe with single colum of chest expansion sequence, filtered and normalised, index is time stamps
+            scale_high - threshold value for high flow rates (via chest expansion rate), used on Insp and Exp
+            scale_low - ratio from max expiration value as max for post expiration pause
+            defaults for scale derived from coordination analysis in Upham 2018
+    Outputs: respPhase - dataframe with index matching input, column per phase component with sig values when phase, np.nan when not
+    Note: Not fast, 0.5 s for 10 minutes of resp.
+    '''
+
+    #the resp phases data frame
+    respPhases = pd.DataFrame(index = sig.index)
+    respPhases['Signal'] = sig.values
+    sig_diff = sig.diff()
+    
+    Breaths = Inspiration_Extract(sig) #respy
+    respPhases['Inspiration_Onset'] = np.nan
+    respPhases['Inspiration_Onset'].loc[Breaths.In]=1
+    respPhases['Expiration_Onset'] = np.nan
+    respPhases['Expiration_Onset'].loc[Breaths.Ex]=1
+    
+    # full inspiration phase
+    Breaths['In_val'] = True
+    Breaths['Exp_val'] = False
+    inds = sig.index
+    ends = pd.DataFrame(index = [inds[0], inds[-1]],data = [False,False]) 
+    df1 = pd.DataFrame(index = Breaths.In, data = Breaths.In_val.values)
+    df2 = pd.DataFrame(index = Breaths.Ex, data = Breaths.Exp_val.values)
+    onsets = pd.concat([df1,df2,ends])
+    onsets = onsets.loc[onsets.index.sort_values()]
+    f = interp1d(onsets.index,onsets[0], kind='previous')
+    sig_alt=f(sig.index)
+    respPhases['Inspiration'] = np.nan
+    respPhases['Inspiration'].loc[sig_alt==1] = sig.loc[sig_alt==1]
+    respPhases['Inspiration_diff'] = np.nan
+    respPhases['Inspiration_diff'].loc[sig_alt==1] = sig_diff.loc[sig_alt==1]
+
+    # full expiration phase
+    Breaths['In_val'] = False
+    Breaths['Exp_val'] = True
+    df1 = pd.DataFrame(index = Breaths.In, data = Breaths.In_val.values)
+    df2 = pd.DataFrame(index = Breaths.Ex, data = Breaths.Exp_val.values)
+    onsets = pd.concat([df1,df2,ends])
+    onsets = onsets.loc[onsets.index.sort_values()]
+    f = interp1d(onsets.index,onsets[0], kind='previous')
+    sig_alt=f(sig.index)
+    respPhases['Expiration'] = np.nan
+    respPhases['Expiration'].loc[sig_alt==1] = sig.loc[sig_alt==1]
+    respPhases['Expiration_diff'] = np.nan
+    respPhases['Expiration_diff'].loc[sig_alt==1] = sig_diff.loc[sig_alt==1]
+
+    # thresholds set to sequence values
+    percentileThresh = scale_high # default 0.7 pr 70%, reasonable thresh from Upham 2018
+    threshes = respPhases.diff().quantile(percentileThresh)
+    threshes['Expiration'] = respPhases['Expiration'].diff().quantile(1-percentileThresh)
+    threshes['Expiration_diff'] = respPhases['Expiration_diff'].diff().quantile(1-percentileThresh)
+
+    # sequence-wise high flow moments in insp and exp 
+    respPhases['Inspiration_high'] = np.nan
+    respPhases['Inspiration_high'].loc[respPhases['Inspiration_diff']
+                                     >=threshes['Inspiration']]= sig.loc[respPhases['Inspiration_diff']>=threshes['Inspiration']]
+    respPhases['Expiration_high'] = np.nan
+    respPhases['Expiration_high'].loc[respPhases['Expiration_diff']
+                                     <=threshes['Expiration']]= sig.loc[respPhases['Expiration_diff']<=threshes['Expiration']]
+    # breath-wise high flow moments in insp and exp and post expiration interval (low flow)
+    respPhases['Inspiration_V'] = np.nan
+    respPhases['Expiration_V'] = np.nan
+    respPhases['Post_Expiration'] = np.nan
+
+    insps = Breaths['In'].values
+    exps = Breaths['Ex'].values
+    for i in range(len(insps)-1):
+        bsig = sig.loc[insps[i]:exps[i]]
+        inde = high_delta_range(bsig, scale_high) 
+        respPhases['Inspiration_V'].loc[inde] = bsig.loc[inde]
+
+        bsig = sig.loc[exps[i]:insps[i+1]]
+        inde = high_delta_range(bsig, scale_high) 
+        respPhases['Expiration_V'].loc[inde] = bsig.loc[inde]
+        inde = post_exp_pause(bsig,scale_low)
+        respPhases['Post_Expiration'].loc[inde] = bsig.loc[inde]
+    return respPhases
+
+def high_delta_quant(signal_segment, quant):
+    ''' high_v reports the indices of high velocity chest expansion/contraction
+    on a (binary) phase segment of the respiration cycle, insp or exp  
+    signal_segment: timestamp index, single column filtered and normalised chest 
+         stretch for a half cycle of resp (signal_segment = sig.loc[insps[i]:exps[i]])
+    quant: threshold quantile, [0,1], min extreme for differenced signal segment
+    '''
+    if signal_segment.diff().mean()>0: # inspiration phase
+        bsig = signal_segment.copy()
+    else: # expiration phase
+        bsig = -signal_segment.copy()
+        print('exp')
+    a= bsig.diff()
+    a.loc[a<0]=np.nan
+    thresh = a.quantile(quant)
+    ind = bsig.index[a>thresh]
+    return ind
+
+def high_delta(signal_segment, thresh):
+    ''' high_v reports the indices of high velocity chest expansion/contraction
+    on a (binary) phase segment of the respiration cycle, insp or exp  
+    signal_segment: timestamp index, single column filtered and normalised chest 
+         stretch for a half cycle of resp (signal_segment = sig.loc[insps[i]:exps[i]])
+    quant: threshold value, scalar min extreme for differenced signal segment
+    '''
+    if signal_segment.diff().mean()>0: # inspiration phase
+        bsig = signal_segment.copy()
+    else: # expiration phase
+        bsig = -signal_segment.copy()
+        print('exp')
+    a= bsig.diff()
+    ind = bsig.index[a>thresh]
+    return ind
+
+def high_delta_range(signal_segment, scale=0.7):
+    ''' high_v reports the indices of high velocity chest expansion/contraction
+    on a (binary) phase segment of the respiration cycle, insp or exp  
+    signal_segment: timestamp index, single column filtered and normalised chest 
+         stretch for a half cycle of resp (signal_segment = sig.loc[insps[i]:exps[i]])
+    scale: threshold ration of max, scalar min extreme for differenced signal segment
+    '''
+    if signal_segment.diff().mean()>0: # inspiration phase
+        bsig = signal_segment.copy()
+    else: # expiration phase
+        bsig = -signal_segment.copy()
+    a= bsig.diff()
+    max_v = a.max()
+    ind = bsig.index[a>a.max()*scale]
+    return ind
+
+def post_exp_pause(signal_segment, scale=0.3):
+    '''identifies the post expiration pause in the expiration half of the respiration cycle
+    '''
+    ind = []
+    if signal_segment.diff().mean()<0: # expiration phase
+        bsig = signal_segment.copy()
+        a= bsig.diff()
+        min_v = a.min()
+        min_i = a.idxmin()
+        a.loc[:min_i] = min_v
+        ind = bsig.index[a>min_v*scale]
+    return ind
+
+def trimAct(df_Act,quant=0.25):
+    K = np.abs(df_Act).sum(axis=1)
+    thresh = K.quantile(quant)
+    id = K.loc[K > thresh].index
+    df_Trimed = df_Act.loc[id[0]:id[-1],:]
+    return df_Trimed
